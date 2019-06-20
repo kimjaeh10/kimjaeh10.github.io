bc106efaf0db6de66da25bc316e998ae6c863d32
Jaehyung Kim <kimjaeh10@gmail.com>
2019-06-19 21:43:45 -0400

    Updated new version of portfolio

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jay Kim - Portfolio</title>
+	<meta name="description" content="Free Web tutorials">
+    <meta name="keywords" content="Jay Kim, Portfolio, Computer Science, Michigan State University, Software engineering">
+    <meta name="author" content="Jay Kim">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href="https://fonts.googleapis.com/css?family=Catamaran:400,700,900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header>
+		<a href="index.php" class="header-brand">JAY KIM</a>
+
+		<nav>
+			<ul>
+				<li><a href="about.php">ABOUT</a></li>
+				<li><a href="education.php">EDUCATION</a></li>
+				<li><a href="projects.php">PROJECTS</a></li>
+				<li><a href="skills.php">SKILLS</a></li>
+			</ul>
+			<a href="contact.php" class="header-contact">CONTACT</a>
+		</nav>
+	</header>
+	<main>
+		<section class="index-banner">
+			<div class="vertical-center">
+				<h2>I AM AN UNDERGRADUATE<br>COMPUTER SCIENCE STUDENT</h2>
+				<h1>who is interested in fullstack web development, Android application development, and data science</h1>
+			</div>
+		</section>
+		<section class="index-links">
+			<a href="about.php">
+				<div class="index-boxlink">
+					<h3>ABOUT</h3>
+				</div>
+			</a>
+			<a href="education.php">
+				<div class="index-boxlink">
+					<h3>EDUACATION</h3>
+				</div>
+			</a>
+			<a href="projects.php">
+				<div class="index-boxlink">
+					<h3>PROJECTS</h3>
+				</div>
+			</a>
+			<a href="skills.php">
+				<div class="index-boxlink">
+					<h3>SKILLS</h3>
+				</div>
+			</a>
+			<a href="contact.php">
+				<div class="index-boxlink">
+					<h3>CONTACT</h3>
+				</div>
+			</a>
+		</section>
+	</main>
+
+	<footer>
+
+	</footer>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
